067f970c8190dae15b4da38f4b2b7ea0642d32b0
prajwal zaware <prajwalzawarehouse@gmail.com>
2025-06-27 16:41:42 +0530

    changed the route name

--- router/router.go
+++ router/router.go
@@ -10,13 +10,13 @@ func SetupRoutes(app *fiber.App) {
 	app.Get("/ping", controller.Pong)
 
 	app.Post("/url/shorten", middleware.RequireAuth(), middleware.RequireRoleAccess(), controller.ShortenURLHandler)
-	app.Get("/url/redirect/:shortCode", middleware.RequireAuth(), middleware.RateLimitMiddleware, middleware.RequireRoleAccess(), controller.RedirectHandler)
+	app.Get("/:shortCode", middleware.RequireAuth(), middleware.RateLimitMiddleware, middleware.RequireRoleAccess(), controller.RedirectHandler)
 	app.Get("/url/stats", middleware.RequireAuth(), middleware.RequireRoleAccess(), controller.GetStats)
 	app.Get("/url/AllUrls", middleware.RequireAuth(), middleware.RequireRoleAccess(), controller.GetAllUrls)
 	app.Post("/url/deleteShortCode", middleware.RequireAuth(), middleware.RequireRoleAccess(), controller.DeleteShortCode)
 
 	app.Post("/tenant/createNewTenant", middleware.RequireAuth(), middleware.RequireRoleAccess(), controller.CreateNewTenant)
-	
+
 	app.Post("/user/signup", controller.SignUpUser)
 	app.Post("/user/login", controller.LoginUser)
 }
